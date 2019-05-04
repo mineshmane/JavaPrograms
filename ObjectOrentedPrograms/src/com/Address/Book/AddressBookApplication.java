520a7bce7b88693bb13d84c61f521d0300de1c5a
mineshmane <mineshmane94gmail.com>
2019-05-04 10:10:21 +0530

    -

---
+++ ObjectOrentedPrograms/src/com/Address/Book/AddressBookApplication.java
@@ -0,0 +1,14 @@
+package com.Address.Book;
+
+public class AddressBookApplication {
+
+
+	static boolean value = true;
+		
+		public static void main(String[] args) 
+		{
+		
+			Menu.addressMenu(value);
+	}
+	
+}
